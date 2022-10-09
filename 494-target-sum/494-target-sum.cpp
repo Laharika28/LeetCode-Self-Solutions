b0474980a627b3627e7b1873d3336c7301b9a4bb
Laharika Jain <64603876+Laharika28@users.noreply.github.com>
2022-10-09 13:44:35 +0530

    Time: 11 ms (92.10%), Space: 9 MB (92.94%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,32 +1,64 @@
 class Solution {
-    int countSubsetSum(vector<int>& arr,int n,int sum){
-        int mod = 1000000007;
-        long long t[n+1][sum+1];
-        for(int j=0;j<sum+1;j++){
-            t[0][j]=0;
-        }
-        t[0][0]=1;
-        for(int i=1;i<n+1;i++){
-            for(int j=0;j<sum+1;j++){
-                
-                    if(arr[i-1] <= j){
-                        t[i][j] = ((t[i-1][j-arr[i-1]])%mod+(t[i-1][j])%mod)%mod;
-                    }else{
-                        t[i][j] = t[i-1][j]%mod;
-                    }
+    
+public:
+    int subsetcount(vector<int> arr, int size, int s)
+{
+
+    int t[size + 1][s + 1];
+    for (int i = 0; i < (size + 1); i++)
+
+    {
+        for (int j = 0; j < (s + 1); j++)
+        {
+
+            if (i == 0)
+            {
+                t[i][j] = 0;
+            }
+            if (j == 0)
+            {
+                t[i][j] = 1;
             }
         }
-        return t[n][sum];
     }
-public:
-    int findTargetSumWays(vector<int>& nums, int target) {
-        int sum =0,n=nums.size();
-        for(int i=0;i<n;i++){
-            sum+=nums[i];
+    for (int i = 1; i < (size + 1); i++)
+    {
+        for (int j = 0; j < (s + 1); j++)
+        {
+            if (arr[i - 1] <= j)
+            {
+                t[i][j] = (t[i - 1][j - arr[i - 1]]) + (t[i - 1][j]);
+            }
+            if (arr[i - 1] > j)
+            {
+                t[i][j] = t[i - 1][j];
+            }
         }
-        if(((sum+target)%2 == 1) || (abs(target)>sum))
-            return 0;
-        int s = (sum+target)/2;
-        return countSubsetSum(nums,n,s);
     }
+    return t[size][s];
+}
+   
+   
+   
+   
+    int findTargetSumWays(vector<int>& nums, int target) {
+       
+       
+        int sum=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            sum=sum+nums[i];
+        }
+        int s1;
+        if(abs(target)>sum || ((target+sum) % 2 != 0)){
+            return 0;
+        }
+        s1=(sum-target)/2;
+       
+        int ans;
+        ans=subsetcount(nums,(nums.size()),s1);
+        return ans;
+       
+    }
+
 };
