6ea2edafabbb88fc5b782df2c259ca0c4cff29bd
Laharika Jain <64603876+Laharika28@users.noreply.github.com>
2022-10-31 11:55:56 +0530

    Time: 38 ms (74.97%), Space: 31.1 MB (51.59%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n = nums.size();
+        for(int i=1;i<n;i++){
+            nums[i]+=nums[i-1];
+        }
+        
+        
+        if(nums[n-1]-nums[0] == 0){
+            return 0;
+        }
+        
+        for(int i=1;i<n-1;i++){
+            if(nums[i-1] == nums[n-1]-nums[i]){
+                return i;
+            }
+        }
+        
+        if(nums[n-2]==0){
+            return n-1;
+        }
+        
+        return -1;
+    }
+};
