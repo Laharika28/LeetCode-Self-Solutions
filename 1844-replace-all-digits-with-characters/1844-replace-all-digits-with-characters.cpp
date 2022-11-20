e87cb6514fb98bc2116634a6a59d0893d886ff5d
Laharika Jain <64603876+Laharika28@users.noreply.github.com>
2022-11-20 20:31:19 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (50.16%) - LeetHub

---
+++ 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    
+    
+    //   '1'-'0' 
+    
+    //    49 - 48 = 1
+    
+    
+    string replaceDigits(string s) {
+        
+        string ans;
+        
+        for(int i=0;i<s.size();i++)
+        {
+            if(int(s[i])>=97 && int(s[i])<=122)
+            {
+                ans.push_back(s[i]);
+            }
+            else
+            {
+                char k;
+                k = s[i-1] + (s[i] - '0');
+                ans.push_back(k);
+            }
+            
+            
+        }
+        return ans;
+    }
+};
+
+
