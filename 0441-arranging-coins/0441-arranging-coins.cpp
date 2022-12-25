6f94d4daeea5b7291014c4438be6e21003d8c872
Laharika Jain <64603876+Laharika28@users.noreply.github.com>
2022-12-25 21:38:30 +0530

    Time: 14 ms (28.88%), Space: 5.9 MB (77.70%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cpp
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -4,13 +4,29 @@ public:
         if (n==1) {
             return 1;
         }
-        long long int sum = 0;
-        for (int i=1;i<=n;i++) {
-            sum += i;
-            if (sum > n) {
-                return i-1;
+        // long long int sum = 0;
+        // for (int i=1;i<=n;i++) {
+        //     sum += i;
+        //     if (sum > n) {
+        //         return i-1;
+        //     }
+        // }
+        
+        long long int x = 1;
+        long long int count = 0;
+        
+        while (x<=n) {
+            long long int y;
+            y = x*(x+1)/2;
+            if (y<=n) {
+                count++;
+            } 
+            else {
+                break;
             }
+            x++;
         }
-        return -1;
+        
+        return count;
     }
 };
